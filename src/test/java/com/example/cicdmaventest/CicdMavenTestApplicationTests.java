c977aab43f16b4d3a5facf73b30daef32108d899
choiseonkang <sk0726@oracleadinfo.com>
2023-03-27 09:59:14 +0900

    Initial commit

---
+++ src/test/java/com/example/cicdmaventest/CicdMavenTestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.cicdmaventest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CicdMavenTestApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
