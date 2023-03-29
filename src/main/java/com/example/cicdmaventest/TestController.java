10e8c4486bae063f112dca57e4f39d919b018ed5
choiseonkang <sk0726@oracleadinfo.com>
2023-03-29 09:29:03 +0900

    feat : health check

---
+++ src/main/java/com/example/cicdmaventest/TestController.java
@@ -0,0 +1,13 @@
+package com.example.cicdmaventest;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class TestController {
+
+    @GetMapping("/health-check")
+    public boolean healthCheck() {
+        return true;
+    }
+}
